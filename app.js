b256529b66b10a9a78e43424a6b08a34dfadf485
Mateusz Szczukiewicz <szcmat6277@gmail.com>
2023-07-23 21:11:34 +0200

    Refactor of css

--- app.js
+++ app.js
@@ -10,16 +10,22 @@ const listExpenses = document.getElementById('list-expenses')
 const headingIncomes = document.getElementById('heading-incomes')
 const headingExpenses = document.getElementById('heading-expenses')
 
+const balanceValue = document.getElementById('balance-value')
+const budgetError = document.getElementById('budget-error')
+
 let isActive = false;
+let isValid = false;
+let numberOfItems = 0;
+let totalValue = 0;
 
 const switchFinances = () => {
     if (!isActive) {
         budgetSwitch.style.backgroundColor = 'var(--income-color)'
-        plusIcon.style.transform = 'rotate(90deg)'
+        plusIcon.classList.add('budget__icon--isActive');
         isActive = true;
     } else {
         budgetSwitch.style.backgroundColor = 'var(--expense-color)'
-        plusIcon.style.transform = 'rotate(0deg)'
+        plusIcon.classList.remove('budget__icon--isActive');
         isActive = false;
     }
 }
@@ -28,14 +34,22 @@ const addFinance = () => {
     const newFinanceItem = document.createElement('div');
     newFinanceItem.classList.add('list__list')
     const description = budgetDescription.value;
-    const value = budgetValue.value;
+    const value = Number(budgetValue.value);
 
-    if (!isActive) {
+    if (value <= 0 && description === '') {
+        budgetError.style.visibility = 'visible';
+        isValid = true;
+    } else {
+        budgetError.style.visibility = 'hidden';
+        isValid = false;
+    }
+
+    if (!isActive && !isValid) {
         headingExpenses.style.display = 'block'
         newFinanceItem.innerHTML = `
                 <div class="list__item">
                   <p class="item__description">${description}</p>
-                  <p class="item__values item__value--expenses">+ ${value} PLN</p>
+                  <p class="item__values item__value--expenses">- ${value} PLN</p>
                 </div>
                 <div class="item__buttons">
                   <button class="item__button item__button--edit">
@@ -48,26 +62,56 @@ const addFinance = () => {
         `
 
         listExpenses.append(newFinanceItem)
-    } else {
+        numberOfItems++;
+        totalValue -= value;
+
+        budgetDescription.value = '';
+        budgetValue.value = '';
+    } else if (isActive && !isValid) {
         headingIncomes.style.display = 'block'
         newFinanceItem.innerHTML = `
-                <div class="list__item">
-                  <p class="item__description">${description}</p>
-                  <p class="item__values item__value--incomes">+ ${value} PLN</p>
-                </div>
+            <div class="list__item">
+                <p class="item__description">${description}</p>
+                <p class="item__values item__value--incomes">+ ${value} PLN</p>
                 <div class="item__buttons">
-                  <button class="item__button item__button--edit">
-                    <img class="item__icon" src="assets/editIcon.svg" alt="Edit icon">
-                  </button>
-                  <button class="item__button item__button--delete">
-                    <img class="item__icon" src="assets/deleteIcon.svg" alt="Delete icon">
-                  </button>
+                    <button class="item__button item__button--edit">
+                        <img class="item__icon" src="assets/editIcon.svg" alt="Edit icon">
+                    </button>
+                    <button class="item__button item__button--delete">
+                        <img class="item__icon" src="assets/deleteIcon.svg" alt="Delete icon">
+                    </button>
                 </div>
+            </div>
         `
 
         listIncomes.append(newFinanceItem)
+        numberOfItems++;
+        totalValue += value;
+
+        budgetDescription.value = '';
+        budgetValue.value = '';
     }
+
+    if (numberOfItems === 0) {
+        headingExpenses.style.display = 'none';
+        headingIncomes.style.display = 'none';
+    }
+
+    balanceValue.innerText = totalValue >= 0 ? `+${totalValue} PLN` : `${totalValue} PLN`
+    balanceValue.style.color = totalValue >= 0 ? `var(--income-color)` : `var(--expense-color)`
 }
+
+
 
 budgetSwitch.addEventListener('click', switchFinances)
 budgetCheck.addEventListener('click', addFinance)
+budgetDescription.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        addFinance();
+    }
+});
+budgetValue.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        addFinance();
+    }
+});
